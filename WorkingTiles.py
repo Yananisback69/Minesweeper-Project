5db8ee519e89e23ce9e531e2e11111384f536cec
YaYeet33 <159903209+YaYeet33@users.noreply.github.com>
2024-02-19 18:03:42 +1100

    Update WorkingTiles.py

--- WorkingTiles.py
+++ WorkingTiles.py
@@ -3,9 +3,9 @@ import random
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((800, 800)) # Creating the screen
 pygame.display.set_caption('Minesweeper')
-num_mines = 8
+num_mines = 40
 
 class TileFunction:
     def __init__(self, x, y, image, w, h):
@@ -14,6 +14,7 @@ class TileFunction:
         self.rect.topleft = (x, y)
         self.state = "normal"
         self.is_mine = False
+        self.adjacent_mines = 0
 
     def draw(self, surface):
         action = False
@@ -27,8 +28,10 @@ class TileFunction:
             if pygame.mouse.get_pressed()[0] == 1 and self.state == "normal":
                 if self.is_mine:
                     self.image = bomb_tile  # Change to bomb_tile if it's a mine
+                elif self.adjacent_mines > 0:
+                    self.image = number_tiles[self.adjacent_mines - 1]  # Change to number_tile based on adjacent mines.-1 is there to match the image from the list 'number_tiles'
                 else:
-                    self.image = clicked_tile  # Change to clicked_tile
+                    self.image = clicked_tile
                 self.image = pygame.transform.scale(self.image, (100, 100))  # resizing the image
                 self.state = "clicked"  # now can't click it again
                 action = True
@@ -39,13 +42,22 @@ class TileFunction:
         return action
 
 
-
 tile_size = 100
 tile_gap = 1  # this is how you can tell the tiles apart
 normal_tile = pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/tile1.png")
-bomb_tile = pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/mine placeholder.png")
+bomb_tile = pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/mine final.png")
 flag_tile = pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/flag tile.png")
 clicked_tile = pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/tile broken.png")
+number_tiles = [
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/1 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/2 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/3 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/4 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/5 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/6 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/7 tile.png"),
+    pygame.image.load("C:/Users/Akshat/Documents/Project/pythonProject/assets/8 tile.png")
+]
 
 tiles = []
 for row in range(8):
@@ -53,14 +65,23 @@ for row in range(8):
         tile = TileFunction(col * (tile_size + tile_gap), row * (tile_size + tile_gap), normal_tile, tile_size, tile_size)
         tiles.append(tile)
 
-def place_mines(tiles, num_mines):
-    mine_positions = random.sample(range(len(tiles)), num_mines) #Generates random position from sample of tiles for mine placement
+def mines_and_numbers(tiles, num_mines):
+    mine_positions = random.sample(range(len(tiles)), num_mines)  # Generates random position from sample of tiles for mine placement
     for pos in mine_positions:
-        tiles[pos].is_mine = True #Changes tile state to be a mine
+        tiles[pos].is_mine = True  # Changes tile state to be a mine
 
-# Randomly place mines
-place_mines(tiles, num_mines)
+    for tile in tiles:
+        # Check adjacent tiles and update adjacent_mines
+        row, col = tile.rect.y // (tile_size + tile_gap), tile.rect.x // (tile_size + tile_gap) # Determines grid position of tiles
+        for i in range(-1, 2): # Creates a 3*3 grid
+            for j in range(-1, 2):
+                if 0 <= row + i < 8 and 0 <= col + j < 8:   # Checks to see if neighbouring tile is within the screen boundary
+                    index = (row + i) * 8 + (col + j)   # Calculates index of neighbouring tile
+                    if tiles[index].is_mine:    # Checks if neighbouring tile is a mine
+                        tile.adjacent_mines += 1    # Increments current tile adjacent_mines attribute by 1
 
+# Randomly place mines and assort numbers
+mines_and_numbers(tiles, num_mines)
 
 while True:
 
